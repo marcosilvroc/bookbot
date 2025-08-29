d649573df5d2ca85916e6ea1c26c845377c414bd
marcosilvroc <marco.silvroc@gmail.com>
2025-08-29 01:30:08 -0300

    Updated Bookbot functions

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 from stats import get_num_words, get_num_char, sort_dict, print_bookbot
+import sys
 
 def get_book_text(filepath):
     with open(filepath, encoding="utf-8") as f:
         return f.read()
 
-    return None
-
 def main():
-    text = get_book_text("books/frankenstein.txt")
-    """
-    Sanity check
-    print(len(text))           # total characters
-    print(text[:100])          # start
-    print(text[-100:])         # end
-    """
-    words = get_num_words(text)
-    dict = sort_dict(get_num_char(text))
-    print_bookbot("books/frankenstein.txt", words, dict)
-    return
+    try:
+        if(len(sys.argv) == 2):
+            text = get_book_text(sys.argv[1])
+            """
+            Sanity check
+            print(len(text))           # total characters
+            print(text[:100])          # start
+            print(text[-100:])         # end
+            """
+            words = get_num_words(text)
+            dict = sort_dict(get_num_char(text))
+            print_bookbot(sys.argv[1], words, dict)
+        else:
+            print("Usage: python3 main.py <path_to_book>")
+            sys.exit(1)
+    except FileNotFoundError:
+        print("File not found! Verify your <path_to_book>.")
+        sys.exit(1)
+    except PermissionError:
+            print(f"Error: Permission denied! Please check your privilege in the system.")
+            sys.exit(1)
 
 main()
